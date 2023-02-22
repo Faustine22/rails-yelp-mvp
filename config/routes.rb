e8346770dc5138a5dfab87bea134445995760a82
faustine pena <marlau.p@hotmail.fr>
2023-02-22 20:05:45 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
+end
   # Defines the root path route ("/")
   # root "articles#index"
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+la_mamma = {name: "la mamma", address: "10 rue victor hugo,13100 aix en provence", category: "italian"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "japanese"}
+ciel = {name: "ciel", address: "17 Rue Haxo, 13001 Marseille", category: "chinese"}
+la_marmite = {name: "la marmite", address: "10 Rue Venture, 13001 Marseille", category: "italian"}
+brasserie_debussy = {name: "brasserie debussy", address: "cours estienne d'orves, 13001 Marseille", category:"belgian" }
+
+
+[ pizza_east, ciel, la_marmite, brasserie_debussy].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
